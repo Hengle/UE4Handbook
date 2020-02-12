185cd154cdbd18b0d43b4a72ce3b7b108132c01f
Sui <passed@163.com>
2020-02-13 01:49:43 +0800

    Project Base

---
+++ Projects/Project1/Source/Project1Editor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Project1EditorTarget : TargetRules
+{
+	public Project1EditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "Project1" } );
+	}
+}
